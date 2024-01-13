ac9b21c77d4251eef39605e6c6d62af0b4c9c3e6
Harsh Bhatt <55428431+harshbhatt99@users.noreply.github.com>
2024-01-12 20:47:33 -0500

    Delete assignment_01 directory

--- assignment_01/ex1_test.py
+++
@@ -1,5 +0,0 @@
-import harsh_bhatt as name_point
-
-def test_hi_my_name_is():
-    assert len(name_point.hi_my_name_is())  > 1 
-
--- assignment_01/harsh_bhatt.py
+++
@@ -1,2 +0,0 @@
-def hi_my_name_is():
-    return "Harsh Bhatt" # Write your name inside the quotation marks
